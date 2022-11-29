acb27f4ec014fb3dcfa829da572bd3f233ab2c18
codanaut <cody@codanaut.com>
2022-11-29 01:03:39 -0700

    first attempt at github pages

---
+++ server-info-scripts.js
@@ -0,0 +1,69 @@
+function formdata(){
+  var searchText= document.getElementById("searchText").value;
+
+  var apiPath = "/api/v1/instance"
+  
+  try {
+    var domain = (new URL(searchText))
+    var url = domain.protocol + "//" + domain.hostname + apiPath
+  }
+  catch(err) {
+    try {
+      var fixDomain = "https:" + "//" + searchText
+      var newdomain = (new URL(fixDomain))
+      var url = newdomain.protocol + "//" + newdomain.hostname + apiPath
+    }
+    catch(err) {
+      document.getElementById("message").innerHTML = "Catch inner: " + err.message;
+    }
+    //document.getElementById("message").innerHTML = "Catch 1 : " + err.message;
+  }
+  
+  console.log(url)
+  
+  getUrl(url)
+    .then((data) => {
+      console.log(data);
+      let serverName = data.uri
+      let description = data.short_description
+      let version = data.version
+      let users = data.stats.user_count
+      let status_count = data.stats.status_count
+      let domain_count = data.stats.domain_count
+      let registrations = data.registrations
+      let approval_required = data.approval_required
+      let invites_enabled = data.invites_enabled
+      document.getElementById("serverName").innerHTML = serverName;
+      document.getElementById("description").innerHTML = description;
+      document.getElementById("version").innerHTML = "Server Version: " + version;
+      document.getElementById("user_count").innerHTML = "Users: " + users + " ~ ";
+      document.getElementById("status_count").innerHTML = "Status Count: " + status_count + " ~ ";
+      document.getElementById("domain_count").innerHTML = "Domain Count: " + domain_count;
+      document.getElementById("registrations").innerHTML = "Registrations Enabled: " + registrations + " ~ ";
+      document.getElementById("approval_required").innerHTML = "Approval Required: " + approval_required + " ~ ";
+      document.getElementById("invites_enabled").innerHTML = "Invites Enabled: " + invites_enabled;
+    })
+    .catch((err) => console.log(err));
+
+  }
+  
+  // make get request
+  function getUrl(url) {
+    return fetch(url) // return this promise
+      .then((response) => response.json());
+  }
+  
+  
+  // Enter on Submit
+  var input = document.getElementById("searchText");
+  
+  // Execute a function when the user presses a key on the keyboard
+  input.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("enterBtn").click();
+    }
+  });
